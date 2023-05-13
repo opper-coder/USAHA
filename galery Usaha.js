d7687144e4eb9ed6f15f7844f898f88c6c06d8bf
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-05-14 07:14:02 +0800

    Create galery Usaha.js

---
+++ galery Usaha.js
@@ -0,0 +1,3 @@
+Galeri usaha adalah membuatkan sebuah konsep serta peralatan yang di sediakan untuk masyarakat
+bagi yang berminat silahkan menggunakan fasilitas dari kami
+kita lakukan MOU dengan berikan konsep, fasilitas dan beberapa IDE dan kita mendapatkan Saham 
